4e0fe10afc66cb8ad67a4a957dabd70842857268
Daniel Dinh <danieldinh4112@gmail.com>
2025-05-28 16:38:45 -0700

    Updating the project to be the correct type of project React App -> Next.js project

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+import '../styles/globals.css';
+
+export default function Home() {
+  return (
+    <>
+    <div className="body">
+      <h1>
+      Under Construction!
+      </h1>
+      <p>
+        This page is currently under construction.
+      </p>
+      <p>
+        Please keep coming back to see how this page develops!
+      </p>
+    </div>
+    </>
+  );
+}
