2d6b106814acca31c62415829717987148e0f11b
Daniel Dinh <danieldinh4112@gmail.com>
2025-05-28 16:05:37 -0700

    Updated the main page to have a current under construction page. This will be changed to be it's own seperate page in the near future

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <h1>Under Construction!</h1>
+
+    <p>This site is currently under construction.</p>
+    <p>Please be patient as I continue working on it</p>
   </StrictMode>,
 )
