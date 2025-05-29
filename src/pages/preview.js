b22da5c92f48cefb48fdeb9ae80e27abcc9f527a
Daniel Dinh <danieldinh4112@gmail.com>
2025-05-29 14:22:16 -0700

    Update to fix error

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -2,7 +2,7 @@ export default function Preview() {
   return (
     <div>
       <h1>My Real Homepage</h1>
-      <p>This is where I'm developing the real version of the site.</p>
+      <p>This is where I am developing the real version of the site.</p>
     </div>
   );
 }
